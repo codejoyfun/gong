44f0b81e639d1f5ef34ad3bea2c39f22724fba1c
libin_zone <libin31811@126.com>
2017-06-26 18:32:03 +0800

    tabbar

--- app/src/main/java/com/runwise/supply/MainActivity.java
+++ app/src/main/java/com/runwise/supply/MainActivity.java
@@ -23,15 +23,46 @@ import com.runwise.supply.business.NearFragment;
 import com.runwise.supply.entity.UserInfo;
 import com.runwise.supply.index.IndexFragment;
 import com.runwise.supply.mine.MineFragment;
+import com.socketmobile.capture.Capture;
+import com.socketmobile.capture.client.CaptureClient;
+import com.socketmobile.capture.client.CaptureDeviceClient;
+import com.socketmobile.capture.events.DataDecodedEvent;
+import com.socketmobile.capture.events.DeviceAvailabilityEvent;
+import com.socketmobile.capture.types.DecodedData;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.Collection;
 
 public class MainActivity extends NetWorkActivity {
-
+    private int devicesConnected = -1;
     private long mExitTime;
     @ViewInject(android.R.id.tabhost)
     private FragmentTabHost mTabHost;
     //未读小红点
     private TextView mMsgHite;
     private UserInfo userInfo;
+    private CaptureClient.Listener mListener= new CaptureClient.Listener()
+    {
+
+
+        @Override
+        public void onConnectionFailure(int i) {
+
+        }
+
+        @Override
+        public void onDeviceAvailabilityChanged(CaptureDeviceClient captureDeviceClient) {
+
+        }
+
+        @Override
+        public void onDataDecoded(CaptureDeviceClient captureDeviceClient, DecodedData decodedData) {
+            print(decodedData.getString());
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +73,10 @@ public class MainActivity extends NetWorkActivity {
         //检查版本
         CheckVersionManager checkVersionManager = new CheckVersionManager(this);
 //        checkVersionManager.checkVersion(false);
-
+        Capture.init(getApplicationContext());
+//        CaptureClient mClient = new CaptureClient();
+//        mClient.setListener(mListener);
+//        mClient.connect();
     }
 
 
@@ -67,10 +101,24 @@ public class MainActivity extends NetWorkActivity {
     }
 
     private void initTabView() {
+        //TODO:这里根据登录状态，设置不同的页面进去
         mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
-        mTabHost.addTab(createTabSpace(R.drawable.tab_1_selector, R.string.tab_1), IndexFragment.class, null);
-        mTabHost.addTab(createTabSpace(R.drawable.tab_2_selector, R.string.tab_2), NearFragment.class, null);
-        mTabHost.addTab(createTabSpace(R.drawable.tab_4_selector, R.string.tab_3), MineFragment.class, null);
+        if (!isLogined()){
+            mTabHost.addTab(createTabSpace(R.drawable.tab_1_selector, R.string.tab_1), IndexFragment.class, null);
+            mTabHost.addTab(createTabSpace(R.drawable.tab_2_selector, R.string.tab_2), NearFragment.class, null);
+            mTabHost.addTab(createTabSpace(R.drawable.tab_3_selector, R.string.tab_3), NearFragment.class, null);
+            mTabHost.addTab(createTabSpace(R.drawable.tab_4_selector, R.string.tab_4), NearFragment.class, null);
+            mTabHost.addTab(createTabSpace(R.drawable.tab_5_selector, R.string.tab_5), MineFragment.class, null);
+        }else{
+            mTabHost.addTab(createTabSpace(R.drawable.tab_1_selector, R.string.tab_1), IndexFragment.class, null);
+            mTabHost.addTab(createTabSpace(R.drawable.tab_2_selector, R.string.tab_2), NearFragment.class, null);
+            mTabHost.addTab(createTabSpace(R.drawable.tab_4_selector, R.string.tab_3), MineFragment.class, null);
+        }
+
+    }
+
+    private boolean isLogined() {
+        return false;
     }
 
     @TargetApi(Build.VERSION_CODES.DONUT)
@@ -112,8 +160,8 @@ public class MainActivity extends NetWorkActivity {
     @Override
     protected void onResume() {
         super.onResume();
-    }
 
+    }
     @Override
     protected void onStop() {
         super.onStop();
@@ -140,5 +188,56 @@ public class MainActivity extends NetWorkActivity {
 
     @Override
     public void onUserLoginout() {
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onScan(DataDecodedEvent event) {
+        print(event.data.getData().toString());
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onDataDecoded(DataDecodedEvent event) {
+        // Do something
+        print(event.data.getData().toString());
+    }
+    @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
+    public void onCaptureDeviceAvailabilityChanged(DeviceAvailabilityEvent event) {
+        Collection<CaptureDeviceClient> devices;
+        updateDeviceState(event);
+
+        // The first time we receive this event - via sticky event - we
+        // want to configure all available devices
+        if (devicesConnected < 0) {
+            devices = event.getAllDevices();
+        } else {
+            devices = event.getChangedDevices();
+        }
+
+        devicesConnected = event.getDeviceCount();
+
+        for (CaptureDeviceClient device : devices) {
+//            if (device.isMine()) {
+//                // Configuration
+//            }
+        }
+    }
+
+    private void updateDeviceState(DeviceAvailabilityEvent event) {
+//        Button btn = (Button) findViewById(R.id.btn_toggle);
+        if (event.isAnyDeviceAvailable()) {
+            print("Device available");
+
+        } else if (event.isAnyDeviceConnected()) {
+            print("Device connected");
+
+        } else {
+            print("No device");
+
+        }
+    }
+
+    private void print(String message) {
+        ToastUtil.show(mContext,message);
+//        ((TextView) findViewById(R.id.hello_scan)).append("\n" + message);
     }
 }
