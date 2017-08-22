58b5226f76dbd6daf457186036994a531e3883fd
1570240728@qq.com <157024>
2017-08-22 17:55:34 +0800

    收货界面调整

--- app/src/main/java/com/runwise/supply/firstpage/ReceiveFragment.java
+++ app/src/main/java/com/runwise/supply/firstpage/ReceiveFragment.java
@@ -6,6 +6,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
@@ -36,7 +37,7 @@ import java.util.Map;
  * Created by libin on 2017/7/16.
  */
 
-public class ReceiveFragment extends BaseFragment{
+public class ReceiveFragment extends BaseFragment {
     private DoActionCallback callback;
 
     public void setCallback(DoActionCallback callback) {
@@ -50,9 +51,10 @@ public class ReceiveFragment extends BaseFragment{
     //跟自己类型配对的数据即可。
     private ArrayList<OrderResponse.ListBean.LinesBean> datas = new ArrayList<>();
     //存放该类型下用户输入的值
-    private Map<String,ReceiveBean> countMap = new HashMap<>();
+    private Map<String, ReceiveBean> countMap = new HashMap<>();
     //收点货模式
     private int mode;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,11 +67,11 @@ public class ReceiveFragment extends BaseFragment{
         ArrayList<OrderResponse.ListBean.LinesBean> linesList = getArguments().getParcelableArrayList("datas");
         mode = getArguments().getInt("mode");
 
-        if (type == DataType.ALL){
+        if (type == DataType.ALL) {
             datas.addAll(linesList);
-        }else {
-            for (OrderResponse.ListBean.LinesBean bean : linesList){
-                if (bean.getStockType().equals(type.getType())){
+        } else {
+            for (OrderResponse.ListBean.LinesBean bean : linesList) {
+                if (bean.getStockType().equals(type.getType())) {
                     datas.add(bean);
                 }
             }
@@ -77,15 +79,17 @@ public class ReceiveFragment extends BaseFragment{
         adapter.setData(datas);
 
     }
+
     @Override
     protected int createViewByLayoutId() {
         return R.layout.reveive_fragment;
     }
+
     @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onCountSynEvent(ReceiveProEvent event){
+    public void onCountSynEvent(ReceiveProEvent event) {
         ReceiveActivity activity = (ReceiveActivity) getActivity();
-        Map<String,ReceiveBean> map = activity.getCountMap();
-        if (map != null){
+        Map<String, ReceiveBean> map = activity.getCountMap();
+        if (map != null) {
             countMap.clear();
             countMap.putAll(map);
         }
@@ -94,20 +98,21 @@ public class ReceiveFragment extends BaseFragment{
 
     public class ReceiveAdapter extends IBaseAdapter {
         private boolean isSettle;       //是不是双单位
+
         @Override
         protected View getExView(int position, View convertView, ViewGroup parent) {
             ViewHolder viewHolder = null;
             final OrderResponse.ListBean.LinesBean bean = (OrderResponse.ListBean.LinesBean) mList.get(position);
             String pId = String.valueOf(bean.getProductID());
-            final ProductBasicList.ListBean basicBean= ProductBasicUtils.getBasicMap(mContext).get(pId);
+            final ProductBasicList.ListBean basicBean = ProductBasicUtils.getBasicMap(mContext).get(pId);
             if (convertView == null) {
                 viewHolder = new ViewHolder();
                 convertView = View.inflate(mContext, R.layout.receive_list_item, null);
                 ViewUtils.inject(viewHolder, convertView);
                 //双人收货模式下，按钮隐藏
-                if (mode == 2){
+                if (mode == 2) {
                     viewHolder.doBtn.setVisibility(View.INVISIBLE);
-                }else{
+                } else {
                     viewHolder.doBtn.setVisibility(View.VISIBLE);
                 }
                 convertView.setTag(viewHolder);
@@ -118,72 +123,72 @@ public class ReceiveFragment extends BaseFragment{
                 @Override
                 public void onClick(View v) {
                     ReceiveBean rb = new ReceiveBean();
-                    if (basicBean != null){
+                    if (basicBean != null) {
                         rb.setName(basicBean.getName());
 //                        rb.setCount((int)bean.getProductUomQty());
                         rb.setCount(0);
                         rb.setProductId(bean.getProductID());
-                        if (isSettle){
+                        if (isSettle) {
                             rb.setTwoUnit(true);
                             rb.setUnit(basicBean.getSettleUomId());
-                        }else{
+                        } else {
                             rb.setTwoUnit(false);
                         }
-                        if (callback != null){
+                        if (callback != null) {
                             callback.doAction(rb);
                         }
                     }
 
                 }
             });
-            if (basicBean != null){
+            if (basicBean != null) {
                 viewHolder.name.setText(basicBean.getName());
                 if (basicBean.getImage() != null)
-                    FrecoFactory.getInstance(mContext).disPlay(viewHolder.sdv, Constant.BASE_URL+basicBean.getImage().getImageSmall());
+                    FrecoFactory.getInstance(mContext).disPlay(viewHolder.sdv, Constant.BASE_URL + basicBean.getImage().getImageSmall());
                 StringBuffer sb = new StringBuffer(basicBean.getDefaultCode());
                 sb.append("  ").append(basicBean.getUnit()).append("\n").append(bean.getPriceUnit()).append("元/").append(bean.getProductUom());
                 viewHolder.content.setText(sb.toString());
-                viewHolder.countTv.setText("/"+(int)bean.getProductUomQty()+basicBean.getUom());
+                viewHolder.countTv.setText("/" + (int) bean.getProductUomQty() + basicBean.getUom());
                 //优先用已输入的数据，没有，则用默认
-                if (mode == 2){
-                    viewHolder.receivedTv.setText(bean.getTallyingAmount()+"");
-                    viewHolder.weightTv.setText(bean.getSettleAmount()+basicBean.getSettleUomId());
-                }else{
-                    if (countMap.containsKey(String.valueOf(bean.getProductID()))){
+                if (mode == 2) {
+                    viewHolder.receivedTv.setText(bean.getTallyingAmount() + "");
+//                    viewHolder.weightTv.setText(bean.getSettleAmount() + basicBean.getSettleUomId());
+                } else {
+                    if (countMap.containsKey(String.valueOf(bean.getProductID()))) {
                         ReceiveBean rb = countMap.get(String.valueOf(bean.getProductID()));
-                        viewHolder.receivedTv.setText(rb.getCount()+"");
-                        viewHolder.weightTv.setText(rb.getTwoUnitValue()+rb.getUnit());
-                    }else{
+                        viewHolder.receivedTv.setText(rb.getCount() + "");
+//                        viewHolder.weightTv.setText(rb.getTwoUnitValue() + rb.getUnit());
+                    } else {
                         viewHolder.receivedTv.setText("0");
-                        viewHolder.weightTv.setText("0"+basicBean.getSettleUomId());
+//                        viewHolder.weightTv.setText("0" + basicBean.getSettleUomId());
                     }
                 }
             }
             //双单位是跟订单相关，所以拿订单里面的字段判断.
-            if (isSettle){
+            if (isSettle) {
                 //显示双单位信息
-                viewHolder.weightTv.setVisibility(View.VISIBLE);
-            }else{
-                viewHolder.weightTv.setVisibility(View.INVISIBLE);
+//                viewHolder.weightTv.setVisibility(View.VISIBLE);
+            } else {
+//                viewHolder.weightTv.setVisibility(View.INVISIBLE);
             }
             return convertView;
         }
-        class ViewHolder{
+
+        class ViewHolder {
             @ViewInject(R.id.productImage)
             SimpleDraweeView sdv;
             @ViewInject(R.id.name)
-            TextView    name;
+            TextView name;
             @ViewInject(R.id.content)
-            TextView    content;
+            TextView content;
             @ViewInject(R.id.countTv)
-            TextView    countTv;
+            TextView countTv;
             @ViewInject(R.id.receivedTv)
-            TextView    receivedTv;
-            @ViewInject(R.id.weightTv)
-            TextView    weightTv;
+            TextView receivedTv;
             @ViewInject(R.id.doBtn)
-            Button      doBtn;
-
+            Button doBtn;
+            @ViewInject(R.id.input_add)
+            ImageButton inputAdd;
 
 
         }
