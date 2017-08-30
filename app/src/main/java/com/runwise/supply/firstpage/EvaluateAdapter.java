2a3f3680dea7a7bde1d84d4e4a5d49e56c885e3e
libin_zone <libin31811@126.com>
2017-08-30 14:49:00 +0800

    评价搜索

--- app/src/main/java/com/runwise/supply/firstpage/EvaluateAdapter.java
+++ app/src/main/java/com/runwise/supply/firstpage/EvaluateAdapter.java
@@ -1,8 +1,14 @@
 package com.runwise.supply.firstpage;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.media.Rating;
 import android.support.v7.widget.RecyclerView;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,6 +35,7 @@ public class EvaluateAdapter extends RecyclerView.Adapter {
     private List<OrderResponse.ListBean.LinesBean> productList = new ArrayList();
     //评价map,LineId ----> 星级
     private Map<Integer,Integer> map;
+    private String filterTxt;          //需要着重显示的文本
     public EvaluateAdapter(Context context,List list,Map<Integer,Integer> map) {
         this.context = context;
         inflater = LayoutInflater.from(context);
@@ -46,8 +53,9 @@ public class EvaluateAdapter extends RecyclerView.Adapter {
         this.callback = callback;
     }
 
-    public void setProductList(List<OrderResponse.ListBean.LinesBean> productList) {
+    public void setProductList(List<OrderResponse.ListBean.LinesBean> productList,String filterTxt) {
         this.productList.clear();
+        this.filterTxt = filterTxt;
         if (productList != null){
             this.productList.addAll(productList);
         }
@@ -68,8 +76,19 @@ public class EvaluateAdapter extends RecyclerView.Adapter {
         final Integer lineId = bean.getSaleOrderProductID();
         ProductBasicList.ListBean basicBean = ProductBasicUtils.getBasicMap(context).get(pId);
         if (basicBean != null){
+            String name = basicBean.getName();
+            if (!TextUtils.isEmpty(filterTxt)){
+                //需要着重显示的
+                int start = name.indexOf(filterTxt);
+                SpannableStringBuilder builder = new SpannableStringBuilder(basicBean.getName());
+                ForegroundColorSpan cs = new ForegroundColorSpan(Color.parseColor("#9ACC35"));
+                builder.setSpan(cs,start,filterTxt.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+                itemHolder.nameTv.setText(builder);
+            }else{
+                itemHolder.nameTv.setText(name);
+            }
             itemHolder.numTv.setText(basicBean.getDefaultCode());
-            itemHolder.nameTv.setText(basicBean.getName());
+
         }
         if (map != null && map.containsKey(lineId)){
             Integer rate = map.get(lineId);
