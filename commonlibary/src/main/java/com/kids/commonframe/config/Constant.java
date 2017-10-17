d17f879be6a8d8181ae176ebfcef3027b24d2efe
1570240728@qq.com <157024>
2017-10-17 18:11:06 +0800

    环境更改

--- commonlibary/src/main/java/com/kids/commonframe/config/Constant.java
+++ commonlibary/src/main/java/com/kids/commonframe/config/Constant.java
@@ -7,11 +7,11 @@ public interface Constant {
     //正式环境地址
 //   public final static String RELEASE_URL = "http://develop.runwise.cn";
 //   public final static String RELEASE_URL = "http://test.runwise.cn";
-   public final static String RELEASE_URL = "http://192.168.0.126:8069";
+//   public final static String RELEASE_URL = "http://192.168.0.126:8069";
 
     boolean test = true;
 
-//    String RELEASE_URL = test?"http://develop.runwise.cn":"http://erp.runwise.cn";
+    String RELEASE_URL = test?"http://develop.runwise.cn":"http://erp.runwise.cn";
 
 
     //测试环境地址
