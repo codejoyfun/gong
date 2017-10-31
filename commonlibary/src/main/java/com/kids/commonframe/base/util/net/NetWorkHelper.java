6b0b136de015f7c268d0634c8ddafa4a8ba0a6d8
1570240728@qq.com <157024>
2017-10-31 16:37:18 +0800

    修改订单送达日期的逻辑 fixbug 下单日期列表跟外部显示不一致 模拟数据

--- commonlibary/src/main/java/com/kids/commonframe/base/util/net/NetWorkHelper.java
+++ commonlibary/src/main/java/com/kids/commonframe/base/util/net/NetWorkHelper.java
@@ -340,6 +340,7 @@ public class NetWorkHelper<T extends BaseEntity> {
 //    public static final String DEFAULT_DATABASE_NAME = "LBZ-Test";
 //    public static final String DEFAULT_DATABASE_NAME = "MF-PreGolive-001";
     public static final String DEFAULT_DATABASE_NAME = "MF-Test";
+//    public static final String DEFAULT_DATABASE_NAME = "LBZ-Golive-01Test";
 //    public static final String DEFAULT_DATABASE_NAME = "";
     // -------------------------------------------------
     private class HttpCallBack<M extends BaseEntity> extends BaseXmlRequest<T> {
