5f78c15124c4cbe4145a314909713cda9cff4dd7
1570240728@qq.com <157024>
2017-11-09 18:37:00 +0800

    develop发包

--- commonlibary/src/main/java/com/kids/commonframe/base/util/net/NetWorkHelper.java
+++ commonlibary/src/main/java/com/kids/commonframe/base/util/net/NetWorkHelper.java
@@ -342,8 +342,8 @@ public class NetWorkHelper<T extends BaseEntity> {
 //    public static final String DEFAULT_DATABASE_NAME = "MF-PreGolive-001";
 //    public static final String DEFAULT_DATABASE_NAME = "";
 //    public static final String DEFAULT_DATABASE_NAME = "LBZ-Golive-01Test";
-//    public static final String DEFAULT_DATABASE_NAME = "ZY-PreGolive-001";
-    public static final String DEFAULT_DATABASE_NAME = "MF-Test";
+    public static final String DEFAULT_DATABASE_NAME = "ZY-PreGolive-001";
+//    public static final String DEFAULT_DATABASE_NAME = "MF-Test";
     // -------------------------------------------------
     private class HttpCallBack<M extends BaseEntity> extends BaseXmlRequest<T> {
         private Class<?> targerClass;
