0d2fc572a372546befca23ca6d04cc3836d73c59
fox.tung <fox.dong@runwise.co>
2017-12-25 15:11:01 +0800

    等待联调

---
+++ app/src/main/java/com/runwise/supply/entity/UpdateTimeRequest.java
@@ -0,0 +1,12 @@
+package com.runwise.supply.entity;
+
+/**
+ * Created by Dong on 2017/12/25.
+ */
+
+public class UpdateTimeRequest {
+    String companyName;
+    public UpdateTimeRequest(String companyName){
+        this.companyName = companyName;
+    }
+}
